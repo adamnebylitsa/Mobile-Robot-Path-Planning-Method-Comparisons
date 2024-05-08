0e669ae6ac705fd11a6228f995778ffec98bef15
kpollackhinds <kahlil.pollack-hinds@stonybrook.edu>
2024-05-08 13:27:57 -0400

    added environment plot, added path finding based on parent to control.py, adjusted in obstacles method in graph.py

--- control.py
+++ control.py
@@ -1,14 +1,22 @@
 from graph import Graph
 import numpy as np
-import random
+from numpy import random
 
-def plan_path(start_point,end_point,robot_radius,environment_grid):
+def plan_path(start_point,end_point,robot_radius,environment_grid, iteration_number = 500):
+    # initialize graph
     graph=Graph()
     graph[start_point]=[]
+    graph["parents"] = {start_point:None}
+
+    path_found = False
+    path = None
+
     grid_shape=environment_grid.shape
     valuesX=np.linspace(0,grid_shape[0]/4,grid_shape[0]+1)
     valuesY=np.linspace(0,grid_shape[1]/4,grid_shape[1]+1)
-    while len(graph)<500:
+
+
+    while len(graph)<iteration_number:
         new_position = (random.choice(valuesX),random.choice(valuesY))
         if Graph.in_obstacle(environment_grid,new_position,robot_radius):
             continue
@@ -18,17 +26,31 @@ def plan_path(start_point,end_point,robot_radius,environment_grid):
             graph[nearest].append(new_position)
             if new_position not in graph:
                 graph[new_position]=[]
+                graph['parents'][new_position] = nearest
             graph[new_position].append(nearest)
-            #
-            #add the parent thing
-            #
-    return graph
+
+            # check if clear path from most recent point to end point
+            if Graph.clear_path(environment_grid, end_point, new_position, robot_radius):
+                graph[new_position].append(end_point)
+                if end_point not in graph:
+                    graph[end_point] = []
+                    graph["parents"][end_point] = new_position
+                graph[end_point].append(new_position)
+                path_found = True
+                break
+            
+    if path_found:
+        path = graph.get_path(start_point, end_point)
+    
+    return graph, path
 
 if __name__=="__main__":
-    env = Graph.createEnvironment("environments/environment5.txt")
-    grid=Graph.occupancyGrid(env)
+    env = Graph.createEnvironment("environments/environment3.txt")
+    grid, ax=Graph.occupancyGrid(env, show=False)
     start=(1,1)
     end=(28,28)
     radius=.5
-    graph =plan_path(start,end,radius,grid)
-    print(graph)
+    graph, path =plan_path(start,end,radius,grid, 2000)
+    graph.showGraph(path, ax)
+    # print(graph)
+    print(path)
--- graph.py
+++ graph.py
@@ -4,14 +4,30 @@ import numpy as np
 class Graph(dict):
 
 
-    def get_path(self):
-        return
+    def get_path(self, start_point, end_point):
+        path = [end_point]
+        current_point = end_point
+
+        while current_point != start_point:
+            parent = self['parents'][current_point]
+            if parent is None:
+                break
+            path.append(parent)
+            current_point = parent
+        
+        path.reverse()
+        print("in func: ",path)
+        return path
     
     def nearest_point(self,point):
         nearest =None
         distance=float('inf')
         # for all the points in the graph find if the current distance is the smallest
-        for k in self:
+        keys = list(self.keys())
+        
+        for k in keys:
+            if k == "parents":
+                continue
             if Graph.distance_point(point,k)<distance:
                 nearest=k
                 distance=Graph.distance_point(point,k)
@@ -20,6 +36,15 @@ class Graph(dict):
     #
     #
     #You can clean/change some of these methods if needed
+
+    def showGraph(self, path, ax):
+        keys = list(self.keys())
+        for k in keys:
+            if k == "parents":
+                continue
+            ax.plot(k[0]*4, k[1]*4, 'go')
+        plt.show()
+        return
     @staticmethod
     def createEnvironment(filename):
         f=open(filename,"r")
@@ -40,10 +65,11 @@ class Graph(dict):
                     points.append([float(temp[0]),float(temp[1])])
                 polygon.append(points)
         f.close()
+        print(polygon)
         return (environment,polygon)
     
     @staticmethod
-    def occupancyGrid(environment):
+    def occupancyGrid(environment, show = False):
         grid=np.zeros([int(environment[0][0]*4),int(environment[0][1]*4)])
         for poly in environment[1]:
             minX=Graph.min_list(poly,0)
@@ -51,7 +77,20 @@ class Graph(dict):
             maxX=Graph.max_list(poly,0)
             maxY=Graph.max_list(poly,1)
             grid[int(minX*4):int(maxX*4),int(minY*4):int(maxY*4)]=1
-        return grid
+
+        # grid[0][2] = 2
+        # print(grid)
+        
+        fig, ax = plt.subplots()
+        ax.imshow(grid.T, cmap='Greys', origin='lower', extent=[0, environment[0][0]*4, 0, environment[0][1]*4])
+        ax.set_xlabel('X coordinate')
+        ax.set_ylabel('Y coordinate')
+
+        if show:
+            ax.set_title('2D Occupancy Grid')
+            plt.show()
+            
+        return grid, ax
 
     @staticmethod
     def displayGrid(grid):
@@ -87,7 +126,14 @@ class Graph(dict):
     
     @staticmethod
     def in_obstacle(grid,position,radius):
-        return 1 in grid[int(position[0]-radius*4):int(position[0]+radius*4),int(position[1]-radius*4):int(position[1]+radius*4)]
+        # if 1 in grid[int((position[0]-radius)*4):int((position[0]+radius)*4),
+        #                  int((position[1]-radius)*4):int((position[1]+radius)*4)]:
+            # print(position)
+        return 1 in grid[int((position[0]-radius)*4):int((position[0]+radius)*4),
+                         int((position[1]-radius)*4):int((position[1]+radius)*4)]
+    
+        return 1 in grid[int(position[0]-radius*4):int(position[0]+radius*4),
+                         int(position[1]-radius*4):int(position[1]+radius*4)]
     
     @staticmethod
     def clear_path(grid,point1,point2,radius):
